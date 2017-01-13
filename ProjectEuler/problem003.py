a607b29d7dc0fd3b92e521744b39ad325bbe74a0
podiumdesu <275910206@qq.com>
2017-01-13 17:07:40 +0800

    problem003

---
+++ ProjectEuler/problem003.py
@@ -0,0 +1,17 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+# 13195的质数因子有5,7,13和29.
+# 600851475143的最大质数因子是多少？
+l =[]
+c =[]
+
+for i in range(2,20):
+    if 20 % i = 0:
+        l=l.append(i)
+for q in l:
+    for n in range(2,q):
+        n % q != 0
+        c=c.append(n)
+    else:
+        pass
+print(max(m,for m in c))
